338e31041d102df0286769001fb1988749e1952e
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-12-22 17:39:15 +0530

    update

--- burn/src/app/about/page.js
+++ burn/src/app/about/page.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from "react";
-const AboutImage = lazy(() => import("../components/ABoutImage"));
+const AboutImage = lazy(() => import("../components/AboutImage"));
 const Navigation = lazy(() => import("../components/Navigation"));
 const AboutEd = lazy(() => import("../components/AboutEd"));
 const EdStory = lazy(() => import("../components/EdStory"));
