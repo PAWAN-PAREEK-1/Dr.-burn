909d6bb96fb235d04ad0cb0b6429117842f65a31
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-12-28 13:22:03 +0530

    contact

--- burn/src/app/components/ContactForm.jsx
+++ burn/src/app/components/ContactForm.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import styles from "../style/contactForm.module.css";
 
-const ContactForm = () => {
+
+const ContactForm = ( {isContactPage}) => {
   return (
     <div>
 
-      <div className={styles.hero}>
+      <div className={`${styles.hero} ${isContactPage && styles.Cpage}`}>
 
-          <div className={`${styles.contactForm}`}>
+          <div className={`${styles.contactForm} `}>
 
-            <h2>Hire Me to Speak</h2>
+            <h2 className={isContactPage && styles.hide}>Hire Me to Speak</h2>
+            <h2 className={!isContactPage && styles.hide}>Connect with ed mylett</h2>
 
             <form action="">
-              <input type="text" placeholder="Your Name" />
-              <input type="email" placeholder="Your Email" />
-              <input type="text" placeholder="Company Name" />
-              <input type="text" placeholder="Subject of inquiry" />
-              <textarea name="request" id="" cols="30" rows="5" placeholder='Request'></textarea>
-              <button>Send A Booking Request</button>
+              <input type="text" placeholder="Your Name" className={isContactPage && styles.inputs}/>
+              <input type="email" placeholder="Your Email" className={isContactPage && styles.inputs} />
+              <input type="text" placeholder="Company Name" className={isContactPage && styles.inputHide} />
+              <input type="text" placeholder="Subject of inquiry" className={isContactPage && styles.inputHide} />
+              <textarea name="request" id="" cols="30" rows="5" placeholder='Request' className={isContactPage && styles.inputs}></textarea>
+              <button className={isContactPage && styles.hide}>Send A Booking Request</button>
+              <button className={!isContactPage && styles.hide}>Submit</button>
             </form>
 
           </div>
---
+++ burn/src/app/contact/page.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styles4 from "../style/hire.module.css";
+import styles5 from "../style/home.module.css";
+
+import styles3 from "../style/podcastPage.module.css";
+import Navigation from "../components/Navigation";
+import styles from "../style/contactPage.module.css";
+import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
+
+const page = () => {
+  return (
+    <div>
+      <div className={` ${styles3.mainHero} ${styles.mainHero}`}>
+        <div className={` ${styles3.hero} ${styles.hero}`}>
+          <Navigation />
+          <div
+            className={`${styles5.heroMain} ${styles4.heroMain} ${styles3.heroMain} ${styles.heroMain}`}
+          >
+            <button>Connect</button>
+          </div>
+        </div>
+
+        <div className={styles.textBox}>
+          <h3>
+            Our team gets hundreds of emails, notes and requests for different
+            opportunities every day. Yet we do our best to answer most of your
+            questions in a timely manner as every message is really important to
+            us.
+          </h3>
+        </div>
+      </div>
+
+
+            <div className={styles.contact}>
+                <div className={styles.left}>
+                        <img src="https://www.edmylett.com/img/contact/ed.png" alt="" />
+                </div>
+                <div className={styles.left}>
+                        <ContactForm isContactPage={true} />
+                </div>
+
+            </div>
+
+            <Footer/>
+
+
+    </div>
+  );
+};
+
+export default page;
