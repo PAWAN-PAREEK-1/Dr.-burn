fd7c51eecf0a1acba32b909aa282ee9d3b16eb79
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-12-28 14:34:07 +0530

    loading

--- burn/src/app/components/Loading.jsx
+++ burn/src/app/components/Loading.jsx
@@ -1,8 +1,17 @@
 import React from "react"
 
+
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div>
+      <div className="loading">
+      <div className="three-body">
+<div className="three-body__dot"></div>
+<div className="three-body__dot"></div>
+<div className="three-body__dot"></div>
+</div>
+</div>
+      </div>
   )
 }
 
