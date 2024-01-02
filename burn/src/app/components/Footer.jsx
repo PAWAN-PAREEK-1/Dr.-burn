0dfad38663610f173292a013c681494a30cb130b
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2024-01-02 11:14:46 +0530

    update

--- burn/src/app/components/Footer.jsx
+++ burn/src/app/components/Footer.jsx
@@ -2,6 +2,44 @@ import React from "react";
 import styles from "../style/footer.module.css";
 import Link from "next/link";
 
+
+const footerSocial = [
+
+  {
+    link:"https://www.instagram.com/dr.edwardburnsjr?igsh=ODA1NTc5OTg5Nw==",
+    img:"/images/instagram2.svg",
+    name:"Instagram",
+    followers:2.25
+  },
+  {
+    link:"https://www.tiktok.com/@dr.edwardburns?_t=8iavjpZ8fPi&_r=1",
+    img:"	https://www.edmylett.com/icons/footer/tiktok.svg",
+    name:"TikTok",
+    followers:2.25
+  }
+  ,
+  {
+    link:"https://www.facebook.com/profile.php?id=100084015300138&mibextid=LQQJ4d",
+    img:"		https://www.edmylett.com/icons/footer/facebook.svg",
+    name:"Facebook",
+    followers:2.25
+  }
+  ,
+  {
+    link:"https://www.linkedin.com/in/edwardburnsjr?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    img:"	/images/linkdin.svg",
+    name:"linkedin",
+    followers:2.25
+  }
+  ,
+  {
+    link:"https://x.com/edwardburnsjr27/",
+    img:"	/images/twitter2.svg",
+    name:"Twitter",
+    followers:2.25
+  }
+]
+
 const Footer = () => {
   return (
     <>
@@ -16,18 +54,22 @@ const Footer = () => {
               coaching others on what it takes to become a champion!
             </p>
 
-            <Link href="#">
+
+    {footerSocial.slice(0,3).map((social,i)=>(
+            <Link href={social.link} key={i}>
               {" "}
               <div className={styles.social}>
                 <div className={styles.imgSocial}>
-                  <img src="/images/instagram2.svg" alt="" />
+                  <img src={social.img} alt="" />
                   <div className={styles.detail}>
-                    <h4>instagram</h4>
-                    <h6>2.2M Followers</h6>
+                    <h4>{social.name}</h4>
+                    <h6>{social.followers} Followers</h6>
                   </div>
                 </div>
               </div>
             </Link>
+            ))}
+
           </div>
           <div className={`${styles.footer} ${styles.footer2} `}>
             <h1 className="colorPrimary">
@@ -54,18 +96,23 @@ const Footer = () => {
               coaching others on what it takes to become a champion!
             </p>
 
-            <Link href="#">
-              {" "}
-              <div className={styles.social}>
-                <div className={styles.imgSocial}>
-                  <img src="/images/instagram2.svg" alt="" />
-                  <div className={styles.detail}>
-                    <h4>instagram</h4>
-                    <h6>2.2M Followers</h6>
-                  </div>
-                </div>
-              </div>
-            </Link>
+
+
+            {footerSocial.slice(3).map((social,i)=>(
+
+<Link href={social.link} key={i}>
+{" "}
+<div className={styles.social}>
+  <div className={styles.imgSocial}>
+    <img src={social.img} alt="" />
+    <div className={styles.detail}>
+      <h4>{social.name}</h4>
+      <h6>{social.followers} Followers</h6>
+    </div>
+  </div>
+</div>
+</Link>
+            ))}
           </div>
         </div>
         </div>
